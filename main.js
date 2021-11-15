86bdf2337864c3613c3707dc7d214c04fc68c8e1
Tanisha704 <76688253+Tanisha704@users.noreply.github.com>
2021-11-15 19:51:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+noseX=0;
+noseY=0;
+
 function preload(){
+lipstick=loadImage('https://i.postimg.cc/PxFvYgkv/l1.png');
 }
 
 function setup(){
@@ -20,15 +24,17 @@ function gotPoses(results){
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose_x =" + results[0].pose.nose.x);
-        console.log("nose_y =" + results[0].pose.nose.y);
-
+        noseX=results[0].pose.nose.x-20;
+        noseY=results[0].pose.nose.y+5;
+        console.log("nose_x =" + noseX);
+        console.log("nose_y =" + noseY);
 
     }
 }
 
 function draw(){
 image(video,0,0,300,300);
+image(lipstick, noseX, noseY,40,30);
 }
 
 function take_snapshot(){
